8bd4e8d8be791cbb3671c919974492618bec6042
Chojan Shang <chojan.shang@vesoft.com>
2025-10-13 03:11:53 +0800

    fix: make echo work
    Signed-off-by: Chojan Shang <chojan.shang@vesoft.com>

--- examples/echo_agent.py
+++ examples/echo_agent.py
@@ -3,16 +3,30 @@ import asyncio
 from acp import (
     Agent,
     AgentSideConnection,
+    AuthenticateRequest,
+    AuthenticateResponse,
+    CancelNotification,
     InitializeRequest,
     InitializeResponse,
+    LoadSessionRequest,
+    LoadSessionResponse,
     NewSessionRequest,
     NewSessionResponse,
     PromptRequest,
     PromptResponse,
-    SessionNotification,
+    SetSessionModeRequest,
+    SetSessionModeResponse,
     stdio_streams,
+    PROTOCOL_VERSION,
 )
-from acp.schema import TextContentBlock, AgentMessageChunk
+from acp.schema import (
+    AgentCapabilities,
+    AgentMessageChunk,
+    McpCapabilities,
+    PromptCapabilities,
+    SessionNotification,
+    TextContentBlock,
+)
 
 
 class EchoAgent(Agent):
@@ -20,7 +34,17 @@ class EchoAgent(Agent):
         self._conn = conn
 
     async def initialize(self, params: InitializeRequest) -> InitializeResponse:
-        return InitializeResponse(protocolVersion=params.protocolVersion)
+        mcp_caps: McpCapabilities = McpCapabilities(http=False, sse=False)
+        prompt_caps: PromptCapabilities = PromptCapabilities(audio=False, embeddedContext=False, image=False)
+        agent_caps: AgentCapabilities = AgentCapabilities(
+            loadSession=False,
+            mcpCapabilities=mcp_caps,
+            promptCapabilities=prompt_caps,
+        )
+        return InitializeResponse(
+            protocolVersion=PROTOCOL_VERSION,
+            agentCapabilities=agent_caps,
+        )
 
     async def newSession(self, params: NewSessionRequest) -> NewSessionResponse:
         return NewSessionResponse(sessionId="sess-1")
